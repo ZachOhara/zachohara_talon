0df492114fdb49dad59a7eef9352565f508af24c
Zach Ohara <zjohara@gmail.com>
2021-01-21 01:43:53 -0800

    Add a script for easier updating of vocabulary

---
+++ commit_vocabulary_changes.sh
@@ -0,0 +1,6 @@
+git reset
+git add code/homophones.csv
+git add misc/extensions.talon
+git add settings/additional_words.csv
+git add settings/words_to_replace.csv
+git commit -m "Update vocabulary"
